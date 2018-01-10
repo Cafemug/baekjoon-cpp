34ffa9b68309e7080c27309799174572df7e0de6
CAFEMUG <hyanghope@naver.com>
2018-01-11 00:35:24 +0900

    c++

---
+++ 2445.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+using namespace std;
+int main(){
+	int num;
+	cin>>num;
+	for(int k=0;k<num;k++){
+	
+	for(int j=k;j>=0;j--){
+		cout<<"*";
+	}
+	for(int i=k;i<num-1;i++){
+		cout<<" ";
+	}
+	for(int i=k;i<num-1;i++){
+		cout<<" ";
+	}
+	for(int j=k;j>=0;j--){
+		cout<<"*";
+	}
+	cout<<endl;
+	}
+	for(int k=0;k<num-1;k++){
+		
+		for(int s=k;s<num-1;s++){
+			cout<<"*";
+		}
+		for(int i=k;i>=0;i--){
+			cout<<" ";
+		}
+		for(int i=k;i>=0;i--){
+			cout<<" ";
+		}
+		for(int s=k;s<num-1;s++){
+			cout<<"*";
+		}
+		cout<<endl;
+	}
+
+}
