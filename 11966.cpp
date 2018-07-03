d3977a4c973fe008f17911fc34a95cf22f71bb14
Cafemug <hyanghope@naver.com>
2018-07-03 18:02:04 +0900

    c++

---
+++ 11966.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a,temp;
+    cin>>a;
+    while(a>1){
+        if(a%2!=0){
+            cout<<0;
+            break;
+        }
+        else{
+            a/=2;
+        }
+    }
+    if(a<=1)
+        cout<<1;
+}
