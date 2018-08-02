e912bbaabd6206274fb0466b8e73a61f1152ba7c
Cafemug <hyanghope@naver.com>
2018-08-02 16:11:23 +0900

    c++

---
+++ 15552.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    int n,a,b;
+    cin>>n;
+    while(n-->0){
+        cin>>a>>b;
+        cout<<a+b<<"\n";
+    }
+}
