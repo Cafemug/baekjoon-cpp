6bef4fc1b3f0970f8b0739dea58fafbdb8c4ede7
Cafemug <hyanghope@naver.com>
2018-08-06 15:58:13 +0900

    c++

--- 11320.cpp
+++ 11320.cpp
@@ -7,7 +7,7 @@ int main(){
         int a,b;
         cin>>a>>b;
         int c=a/b;
-        if(a*a>b*b*c*c)
+        if(a*a>(b*b*c*c))
             cout<<(c*c)+1<<"\n";
         else
             cout<<(c*c)<<"\n";
