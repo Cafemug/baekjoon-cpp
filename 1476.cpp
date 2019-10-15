0ea0a35f5e352989802e25bd7c157c704ea28505
Jeon Ga Bin <hyanghope@naver.com>
2019-10-15 23:14:33 +0900

    1476 구현중

--- 1476.cpp
+++ 1476.cpp
@@ -1,1 +1,12 @@
 // 날짜계산
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b,c,res=10
+    cin>>a>>b>>c;
+    while(res++){
+        if((res-a)%15==0 && (res-b)%28==0 && (res-c)%19==0)
+            break;
+    }
+    cout<<res;
+}
