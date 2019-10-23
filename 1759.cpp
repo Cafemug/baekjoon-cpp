b29d07d083c60adb738283814cccb22dbb4d8090
Jeon Ga Bin <hyanghope@naver.com>
2019-10-23 22:39:02 +0900

    암호 만들기 구현중

---
+++ 1759.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+char arr[16];
+int a,b,check[16];
+void recur(int idx,int depth){
+    if(depth==a){
+        for(int t=0;t<b;t++){
+            if(check[t])
+                cout<<arr[t];
+        }
+        cout<<"\n";
+    }
+    
+    for(int i=0;i<b;i++){
+        if(!check[i]){
+            check[i]=1;
+            recur(i,depth+1);
+            check[i]=0;
+        }
+    }
+
+}
+int main(){
+    cin>>a>>b;
+    for(int i=0;i<b;i++){
+        cin>>arr[i];
+    }
+    sort(arr,arr+b);
+    for(int i=0;i<=b-a;i++){
+        recur(i,0);
+    }
+
+}
