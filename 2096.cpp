5ad5f00003008befa35969183a9d2af0b718353c
Jeon Ga Bin <hyanghope@naver.com>
2019-10-29 23:20:51 +0900

    메모리 초과2

--- 2096.cpp
+++ 2096.cpp
@@ -10,24 +10,26 @@ int main(){
     int num,maxres,minres;
     cin>>num;
     for(int i=0;i<num;i++){
-        cin>>arr[i][0]>>arr[i][1]>>arr[i][2];
-    }
-    maxd[0][0] = arr[0][0];
-    maxd[0][1] = arr[0][1];
-    maxd[0][2] = arr[0][2];
-    mind[0][0] = arr[0][0];
-    mind[0][1] = arr[0][1];
-    mind[0][2] = arr[0][2];
-    for(int i=1;i<num;i++){
-        maxd[i][0]=max(maxd[i-1][0],maxd[i-1][1])+arr[i][0];
-        maxd[i][1]=max(maxd[i-1][0],max(maxd[i-1][1],maxd[i-1][2]))+arr[i][1];
-        maxd[i][2]=max(maxd[i-1][2],maxd[i-1][1])+arr[i][2];
-        mind[i][0]=min(mind[i-1][0],mind[i-1][1])+arr[i][0];
-        mind[i][1]=min(mind[i-1][0],min(mind[i-1][1],mind[i-1][2]))+arr[i][1];
-        mind[i][2]=min(mind[i-1][2],mind[i-1][1])+arr[i][2];
-
+        int a,b,c;
+        cin>>a>>b>>c;
+        if(i==0){
+            maxd[i][0]=a;
+            maxd[i][1]=b;
+            maxd[i][2]=c;
+            mind[i][0]=a;
+            mind[i][1]=b;
+            mind[i][2]=c;
+        }
+        else{
+            maxd[i][0]=max(maxd[i-1][0],maxd[i-1][1])+a;
+            maxd[i][1]=max(maxd[i-1][0],max(maxd[i-1][1],maxd[i-1][2]))+b;
+            maxd[i][2]=max(maxd[i-1][2],maxd[i-1][1])+c;
+            mind[i][0]=min(mind[i-1][0],mind[i-1][1])+a;
+            mind[i][1]=min(mind[i-1][0],min(mind[i-1][1],mind[i-1][2]))+b;
+            mind[i][2]=min(mind[i-1][2],mind[i-1][1])+c;
+        }
     }
     maxres = max(maxd[num-1][0],max(maxd[num-1][1],maxd[num-1][2]));
-    minres= min(maxd[num-1][0],min(maxd[num-1][1],maxd[num-1][2]));
+    minres= min(maxd[num-1][0],min(maxd[num-1][1],mind[num-1][2]));
     cout<<maxres<<" "<<minres;
 }
