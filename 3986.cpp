4ea871e3e05570d79c3acde46e5c9fb526d70b6c
Jeon Ga Bin <hyanghope@naver.com>
2019-11-17 20:32:54 +0900

    좋은단어 구현중2

--- 3986.cpp
+++ 3986.cpp
@@ -10,18 +10,23 @@ int main(){
         int flag=1;
         cin>>s;
         int len = s.size();
-        for(int i=0;i<len;i++){
-            int vlen = v.size();
-            if(vlen==0 || (vlen ==1 && v[vlen-1]!=s[i])){
-                v.push_back(s[i]);
-                continue;
-            }
-            if(v[vlen-1]==s[i]){
-                v.pop_back();
-            }
-            else{
-                    flag=0;
-                    break;
+        if(len%2!=0){
+            flag=0;
+        }
+        else{
+            for(int i=0;i<len;i++){
+                int vlen = v.size();
+                if(vlen==0 || (vlen ==1 && v[vlen-1]!=s[i])){
+                    v.push_back(s[i]);
+                    continue;
+                }
+                if(v[vlen-1]==s[i]){
+                    v.pop_back();
+                }
+                else{
+                        flag=0;
+                        break;
+                }
             }
         }
         if(flag)
