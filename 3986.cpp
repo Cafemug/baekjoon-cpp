0f44cd5aa7ba20a669e23964cec6920de211f9ce
Jeon Ga Bin <hyanghope@naver.com>
2019-11-18 23:58:43 +0900

    좋은단어 스택써서 풀어보기

--- 3986.cpp
+++ 3986.cpp
@@ -4,33 +4,5 @@ using namespace std;
 int main(){
     int n,count=0;
     cin>>n;
-    while(n--){
-        vector<char> v;
-        string s;
-        int flag=1;
-        cin>>s;
-        int len = s.size();
-        if(len%2!=0){
-            flag=0;
-        }
-        else{
-            for(int i=0;i<len;i++){
-                int vlen = v.size();
-                if(vlen==0 || (vlen ==1 && v[vlen-1]!=s[i])){
-                    v.push_back(s[i]);
-                    continue;
-                }
-                if(v[vlen-1]==s[i]){
-                    v.pop_back();
-                }
-                else{
-                        flag=0;
-                        break;
-                }
-            }
-        }
-        if(flag)
-            count++;
-    }
-    cout<<count;
+
 }
