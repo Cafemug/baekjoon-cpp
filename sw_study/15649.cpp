b9a06a9f9c12dde9e0cf852b380dad2a8c679ca8
Jeon Ga Bin <hyanghope@naver.com>
2019-12-14 21:49:11 +0900

    N과M 1 재귀

---
+++ sw_study/15649.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int n,m;
+int arr[10];
+bool check[10];
+void recur(int x){
+    if(x==m){
+        for(int i=0;i<m;i++){
+            cout<<arr[i]<<" ";
+        }
+        cout<<"\n";
+        return;
+    }
+    for(int i=1;i<=n;i++){
+        if(check[i]) continue;
+        check[i]=true;
+        arr[x]=i;
+        recur(x+1);
+        check[i]=false;
+    }
+}
+int main(){
+    cin>>n>>m;
+    recur(0);
+    
+}
