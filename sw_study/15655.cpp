9324f6fc77803ed8f4c4fb647fb598a483da2143
cafemug <hyanghope@naver.com>
2019-12-18 10:53:00 +0900

    구현중

---
+++ sw_study/15655.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int n,m;
+int arr[10],res[10];
+bool check[10];
+void recur(int x){
+    if(x>n) return;
+    if(x==m){
+        for(int i=0;i<m;i++){
+            cout<<res[i]<<" ";
+        }
+        cout<<"\n";
+        return;
+    }
+    for(int i=x;i<n;i++){
+        if(check[i]) continue;
+        check[i]=true;
+        res[x]=arr[i];
+        recur(x+1);
+        check[i]=false;
+        
+    }
+}
+int main(){
+    cin>>n>>m;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    sort(arr,arr+n);
+    recur(0);
+    
+}
