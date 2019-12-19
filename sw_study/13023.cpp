4a9306267a2229e094f44c0d9ea3ba7076ea9ce6
Jeon Ga Bin <hyanghope@naver.com>
2019-12-20 00:31:53 +0900

    ABCDE 구현중

--- sw_study/13023.cpp
+++ sw_study/13023.cpp
@@ -5,30 +5,35 @@ bool check[2001];
 int num,sen,idx=0;
 vector<pair<int, int > > v[2001];
 void dfs(int n,int x){
+    // cout<<"n "<<n<<" x "<<x<<"\n";
     if(n>num) return;
-    if(n==num && x>=5)idx=1;
-    check[n]=true;
+    if (x>=4){
+        idx=1;
+        return;
+    }
     int len = v[n].size();
     for(int i=0;i<len;i++){
         pair<int, int> p = v[n][i];
         if(check[p.first]) continue;
         check[p.first] = true;
-        v[n][i].second+=1;
-        dfs(p.first, p.second+1);
+        // cout<<p.first<<" first "<<v[n][i].second<<"\n";
+        v[n][i].second=(x+1);
+        // cout<<p.first<<" second "<<(x+1)<<"\n";
+        dfs(p.first, x+1);
+        check[p.first] = false;
+        v[n][i].second=p.second;
 
     }
 
 }
 int main(){
        cin>>num>>sen;
-       int t;
        for(int i=0;i<sen;i++){
            int a,b;
-           t=a;
            cin>>a>>b;
            v[a].push_back(make_pair(b,0));
            v[b].push_back(make_pair(a,0));
        }
-       dfs(t,0);
+       dfs(0,0);
        cout<<idx;
 }
