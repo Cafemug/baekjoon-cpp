b95645f4dc7c1e68fb5bf3e1d8cbf583e745ef45
Jeon Ga Bin <hyanghope@naver.com>
2020-01-21 21:14:26 +0900

    카잉달력 건너뛰기로 구현

--- sw_study/6064.cpp
+++ sw_study/6064.cpp
@@ -1,10 +1,5 @@
 #include <iostream>
 using namespace std;
-int gcd(int a, int b){
-    if(b<=0) return a;
-    else 
-        return gcd(b, a%b);
-}
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
@@ -14,18 +9,13 @@ int main(){
         int flag=0;
         int m,n,x,y;
         cin>>m>>n>>x>>y;
-        int gcd_num = gcd(m,n);
-        int lcm = m*n/gcd_num;
-        for(int i=0;i<lcm/m;i++){
-            int temp_num= m*i+x;
-            for(int j=0;j<lcm/n;j++){
-                if(temp_num == n*j+y){
-                    cout<<temp_num<<"\n";
-                    flag=1;
-                    break;
-                }
+        for(int i=0;i<=n;i++){
+            int temp_num=i*m+(x-1);
+            if(temp_num%n==(y-1)){
+                flag=1;
+                cout<<temp_num+1<<"\n";
+                break;
             }
-            if(flag) break;
         }
         if(flag) continue;
         cout<<-1<<"\n";
