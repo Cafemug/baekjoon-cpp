745dae6c8c5bc1b47414855dffd5382d0c3211c4
Jeon Ga Bin <hyanghope@naver.com>
2020-04-11 00:27:35 +0900

    부분수열의 합 브루트포스 구현

--- sw_study/14225.cpp
+++ sw_study/14225.cpp
@@ -10,6 +10,10 @@ int main(){
     }
     sort(s, s+n);
     sum = s[0];
+    if(sum!=1){
+        cout<<1;
+        return 0;
+    }
     for(int i=1;i<n;i++){
         if(sum+1<s[i]){
             cout<<sum+1;
@@ -18,13 +22,5 @@ int main(){
         sum+=s[i];
     }
     
-    if(sum==1){
-        cout<<sum+1;
-    }
-    else{
-        cout<<1;
-    }
-    
-    
-    
+    cout<<sum+1;
 }
