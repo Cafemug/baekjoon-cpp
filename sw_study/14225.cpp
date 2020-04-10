e2496f59ec50826a17bfea5935ee0c742f1ad150
Jeon Ga Bin <hyanghope@naver.com>
2020-04-10 17:55:44 +0900

    구현중

---
+++ sw_study/14225.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int s[21];
+int main(){
+    int n,sum;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>s[i];
+    }
+    sort(s, s+n);
+    sum = s[0];
+    for(int i=1;i<n;i++){
+        if(sum+1<s[i]){
+            cout<<sum+1;
+            return 0;
+        }
+        sum+=s[i];
+    }
+    
+    if(sum==1){
+        cout<<sum+1;
+    }
+    else{
+        cout<<1;
+    }
+    
+    
+    
+}
