36c41eee699a9211e3a86812d504d68915456a83
Jeon Ga Bin <hyanghope@naver.com>
2020-04-11 00:45:55 +0900

    자와 각도기 브루트포스 구현

---
+++ sw_study/15661.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int b[21][21];
+int main(){
+    int n;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin>>b[i][j];
+        }
+    }
+    for(int i=1;i<n-1;){
+        int j=n-i;
+        
+    }
+}
